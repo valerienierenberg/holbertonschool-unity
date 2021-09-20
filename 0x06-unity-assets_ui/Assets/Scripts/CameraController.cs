8f0651cd2d4ae664592e406f2bf24fc03b23e27f
valerienierenberg <valerie.nierenberg@gmail.com>
2021-09-20 12:55:08 -0500

    Add public bool isInverted

--- 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public bool isInverted;
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,14 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // if isInverted, camera should move up when mouse moves down and vice versa
+        if (isInverted){
+            yaw += speedH * Input.GetAxis("Mouse Y");
+            pitch -= speedV * Input.GetAxis("Mouse X");
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
     }
 }
+
+
